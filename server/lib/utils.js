70fa7eb5fe24d8b53ec6fef6ef84d3c4b403aeb3
yash-251198 <shetyeyash444@gmail.com>
2025-07-13 00:00:26 +0530

    NEW FUNCTION: setTime function to set invoke the cron jobs

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -4,14 +4,18 @@ import { BlackListTokenModel } from "../model/BalckListTokenModel.js"
 dotenv.config()
 const secrete = process.env.JWT_SECRETE
 
-export const createToken = (userId, email) => {
-    return jwt.sign({ email, userId }, secrete, { expiresIn: "3d" })
+export const createToken = (userId) => {
+    return jwt.sign({ userId }, secrete, { expiresIn: "3d" })
 }
 
+//jwt token verifier to prevent the unauthorized access
+//step 1 : fetch the token from request
+//step 2 : check token is present in expired list.
+//step 3 : verify the token and attached the decoded data to req as user.
 export const jwtVerify = async (req, res, next) => {
-    const token = req.cookies.twt;
+    const token = req.cookies.jwt;
 
-    const isTokenExpired = BlackListTokenModel.findOne({ token })
+    const isTokenExpired = await BlackListTokenModel.findOne({ token })
 
     if (isTokenExpired)
         return res.status(401).json({ message: "Unable to process your request try again later" })
@@ -26,4 +30,13 @@ export const jwtVerify = async (req, res, next) => {
         console.log(error)
         res.status(500).send("Internal server error")
     }
+}
+
+
+export const setMealTime = (hours, minutes) => {
+    const date = new Date()
+    date.setHours(hours)
+    date.setMinutes(minutes)
+
+    return date
 }
