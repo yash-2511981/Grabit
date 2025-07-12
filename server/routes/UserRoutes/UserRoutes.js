4f97b05cfc003344812f072ef72689a6b720bf52
yash-251198 <shetyeyash444@gmail.com>
2025-07-13 00:01:09 +0530

    UPDATE:create new route for get-user-info

--- server/routes/UserRoutes/UserRoutes.js
+++ server/routes/UserRoutes/UserRoutes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { register, signIn, signUp } from "../../controller/UserController/AuthController.js";
 import { loginValidation, registerValidation } from "../../middleware/AuthMiddleware.js";
+import { jwtVerify } from "../../lib/utils.js";
+import { getUserInfo } from "../../controller/UserController/UserController.js";
 
 const userRouter = Router();
 
+//authentication routes
 userRouter.post("/signIn", loginValidation, signIn)
 userRouter.post("/register", registerValidation, register)
 userRouter.post("/signUp", signUp)
+
+//other routes
+userRouter.get("/get-user-info", jwtVerify, getUserInfo)
 
 export default userRouter
