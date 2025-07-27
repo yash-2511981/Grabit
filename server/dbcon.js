444cde1bb7dc7219373676d3c8e52ccc3f74f43d
yash-251198 <shetyeyash444@gmail.com>
2025-07-28 00:09:33 +0530

    configured tailwind css and shadcn ui in our project

--- server/dbcon.js
+++ server/dbcon.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URL
 
 export const connectMongo = () => {
     mongoose.connect(url).then((result) => {
-        console.log("server is running on localhost:3000")
+        console.log("Database is connected")
     }).catch((err) => {
         console.log("error while connecting", err)
     });
