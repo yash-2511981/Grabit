ae5c34028ddc6dbf2dca2a91a19bb6ff475a4562
yash-251198 <shetyeyash444@gmail.com>
2025-08-03 02:46:26 +0530

    Updated userslices for global data management and filters management

--- Client/src/store/slices/userSlices.js
+++ Client/src/store/slices/userSlices.js
@@ -1,8 +1,17 @@
-const userSlices = (set) => {
-    return {
-        userInfo: null,
-        setUserInfo: (userInfo) => set({ userInfo })
-    }
-}
+const userSlices = (set) => ({
+    userInfo: null,
+    cartItems: [],
+    vegMode: false,
+    category: "dish",
+    filters: { veg: false, category: "dishes" },
+    setFilter: (category) => set({ category }),
+    setVegMode: (vegMode) => set({ vegMode }),
+    setUserInfo: (userInfo) => set({ userInfo }),
 
-export default userSlices
+    addCartItems: (item) =>
+        set((state) => ({
+            cartItems: [...state.cartItems, item],
+        })),
+});
+
+export default userSlices;
