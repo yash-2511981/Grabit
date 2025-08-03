3ead8b5a40a4fc39b2fa13702eb02ba5351d1b77
yash-251198 <shetyeyash444@gmail.com>
2025-08-03 23:26:40 +0530

    updated usermodel

--- server/model/UserModel.js
+++ server/model/UserModel.js
@@ -2,8 +2,8 @@ import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
-  firstName: { type: String },
-  lastName: { type: String },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   contact: { type: String, required: true },
   password: { type: String, required: true },
   cart: [{
