664bfc8024e4fe6ca6fabaef6b0fbd71c8327bb4
yash-251198 <shetyeyash444@gmail.com>
2025-08-13 00:03:35 +0530

    added a clickable cart icon as cart trigger with cartitem count

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import Profile from "./Profile"
 import Logo from "./ui/logo"
+import CartSheet from "./CartSheet";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -8,7 +9,10 @@ const Navbar = () => {
     return (
         <div className="w-full sm:max-w-7xl flex justify-between mx-auto p-2">
             <Logo onClick={() => navigate("/home")} />
-            <Profile />
+            <div className="flex items-center justify-center gap-4">
+                <CartSheet />
+                <Profile />
+            </div>
         </div>
     )
 }
