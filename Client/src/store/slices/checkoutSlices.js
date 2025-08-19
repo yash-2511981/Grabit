6bc1987bfece8ce8f92893019cd5ad6c43da8f31
yash-251198 <shetyeyash444@gmail.com>
2025-08-19 21:42:12 +0530

    created a checkout page grid layout to manage efficiently on small and large devices

---
+++ Client/src/store/slices/checkoutSlices.js
@@ -0,0 +1,65 @@
+const checkoutSlices = (set, get) => ({
+    orderProducts: [],       // only { product, quantity } for backend order
+    displayProducts: [],     // detailed info to display on ui
+    orderAmount: 0,
+    deliveryCharge: 20,
+    orderAddress: null,
+    paymentMode: "online",
+    isOrderFromCart: false,
+    setCheckoutData: (products, amount, isOrderFromCart) => {
+        const newOrderProducts = products.map((item) => ({
+            product: item._id,
+            quantity: item.quantity,
+        }))
+
+
+        set({
+            orderProducts: newOrderProducts,
+            displayProducts: products,
+            orderAmount: amount,
+            isOrderFromCart
+        })
+    },
+
+    increaseQuantity: (id) => {
+        const orderProducts = [...get().orderProducts]
+        const displayProducts = [...get().displayProducts]
+
+        const index = orderProducts.findIndex((i) => i.product === id)
+        const displayIndex = displayProducts.findIndex((i) => i._id === id)
+
+        if (index !== -1) {
+            orderProducts[index].quantity += 1
+            displayProducts[displayIndex].quantity += 1
+            set({ orderProducts, displayProducts })
+            get().setOrderAmount()
+        }
+    },
+
+    decreaseQuantity: (id) => {
+        const orderProducts = [...get().orderProducts]
+        const displayProducts = [...get().displayProducts]
+
+        const index = orderProducts.findIndex((i) => i.product === id)
+        const displayIndex = displayProducts.findIndex((i) => i._id === id)
+
+        if (index !== -1 && orderProducts[index].quantity > 1) {
+            orderProducts[index].quantity -= 1
+            displayProducts[displayIndex].quantity -= 1
+            set({ orderProducts, displayProducts })
+            get().setOrderAmount()
+        }
+    },
+
+    setOrderAddress: (address) => set({ orderAddress: address }),
+    setTotalAmount: (amount) => set({ orderAmount: amount }),
+    setPaymentMode: (mode) => set({ paymentMode: mode }),
+    setOrderAmount: () => {
+        const products = get().displayProducts;
+        const total = products.reduce((total, item) => total + (item.price * item.quantity), 0)
+        set({ orderAmount: total })
+    }
+})
+
+
+export default checkoutSlices
