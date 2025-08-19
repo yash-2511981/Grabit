ecf6e28728c8fc68eb33d44cbdb1a53216750ce7
yash-251198 <shetyeyash444@gmail.com>
2025-08-19 21:38:01 +0530

    hiddng footer on small screen keeping mobile first ui in mind

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className="w-full bg-gray-200 text-center py-4 sm:block hidden">
+        <footer className="w-full bg-gray-200 text-center py-5 sm:block hidden">
             <div className="sm:max-w-7xl mx-auto px-4">
                 <p className="text-sm text-gray-500">© 2025 Grabit. All rights reserved.</p>
             </div>
