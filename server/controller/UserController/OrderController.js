de8a9ded67a570de6a7f16b84a6eb8d492fbd817
yash-251198 <shetyeyash444@gmail.com>
2025-08-21 01:48:06 +0530

    added crud operations on order model done by user

--- server/controller/UserController/OrderController.js
+++ server/controller/UserController/OrderController.js
@@ -1,12 +1,37 @@
 import { OrderModel } from "../../model/OrderModel.js";
+import { UserModel } from "../../model/UserModel.js";
 
 export const createOrder = async (req, res) => {
     const { id } = req.data;
-    const { orderItems, amount } = req.body
+    const { orderProducts, amount, deliveryCharge, orderAddress, paymentMode, isOrderFromCart, platFormFee, gst } = req.body
 
     try {
-        const newOrder = await OrderModel.create({ user: id, amount, })
-        res.status(200).json({ id: newOrder._id })
+        const orderOptions = {
+            user: id,
+            amount,
+            deliveryCharge,
+            products: orderProducts,
+            address: orderAddress,
+            paymentMode,
+            orderStatus: paymentMode === "online" ? "pending" : "confirm",
+            platFormFee,
+            gst,
+        }
+
+        const order = await (await OrderModel.create(orderOptions)).populate({
+            path: "products.product",
+            module: "product"
+        })
+
+        if (isOrderFromCart) {
+            await UserModel.findByIdAndUpdate(id, { cart: [] })
+        }
+
+        if (paymentMode === "online") {
+            //razorpay order creation
+        }
+
+        res.status(200).json({ order })
 
     } catch (error) {
         console.log(error)
@@ -15,10 +40,21 @@ export const createOrder = async (req, res) => {
 
 }
 
-export const deleteOrder = async () => {
 
-}
 
-export const udpateOrder = async () => {
+export const getOrderDetails = async (req, res) => {
+    const { id } = req.data
+    try {
+        const orders = await OrderModel.find({ user: id }).populate({
+            path: "products.product",
+            module: "product"
+        })
 
-}
+        const pendingOrders = orders.filter((order) => (order.orderStatus !== "delivered" || order.orderStatus !== "cancled"))
+        const completedOrders = orders.filter((order) => (order.orderStatus === "delivered" || order.orderStatus === "cancled"))
+        res.status(200).json({ pendingOrders, completedOrders })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Server error.Try again later")
+    }
+}   
