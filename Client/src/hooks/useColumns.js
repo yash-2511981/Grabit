1673b086a284d0d639faa1d4519ff2fd6465f407
yash-251198 <shetyeyash444@gmail.com>
2025-08-25 17:41:18 +0530

    updated useColumns to manage edge cases

--- Client/src/hooks/useColumns.js
+++ Client/src/hooks/useColumns.js
@@ -2,10 +2,15 @@ import { useEffect, useState } from 'react'
 
 const useColumns = () => {
     const getColumns = (width) => {
-        if (width >= 1280) return 5;
-        if (width >= 1024) return 4;
-        if (width >= 768) return 3;
-        return 2;
+        if (width >= 1280) {
+            return 5
+        } else if (width >= 1024) {
+            return 4
+        } else if (width >= 640) {
+            return 3
+        } else {
+            return 2
+        }
     }
     const [column, setColumn] = useState(
         typeof window !== 'undefined' ? getColumns(window.innerWidth) : 0
@@ -23,7 +28,7 @@ const useColumns = () => {
         handleResize()
         return () => window.removeEventListener('resize', handleResize)
     }, [])
-
+    console.log(column)
     return column;
 }
 
