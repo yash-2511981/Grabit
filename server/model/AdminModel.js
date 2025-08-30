89638e123683e8867a66671981ff0d654c79bddc
yash-251198 <shetyeyash444@gmail.com>
2025-08-30 19:53:23 +0530

    removed server folder tracking from main repo

--- server/model/AdminModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from "mongoose"
-
-const adminSchema = new Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true }
-})
-
-export const AdminModel = mongoose.model("admin", adminSchema)
